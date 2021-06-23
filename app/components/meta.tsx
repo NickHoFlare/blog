f199fbf18b62bae085aa1744ebb464a7f3dface9
Nicholas Ho <terran324@gmail.com>
2021-06-23 21:05:57 +1000

    Fix favicon

--- app/components/meta.tsx
+++ app/components/meta.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
+import { applyBasePathToImage } from '../lib/api';
 
 const Meta = () => {
   return (
@@ -7,29 +8,29 @@ const Meta = () => {
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
+        href={applyBasePathToImage("/favicon/apple-touch-icon.png")}
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon/favicon-32x32.png"
+        href={applyBasePathToImage("/favicon/favicon-32x32.png")}
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon/favicon-16x16.png"
+        href={applyBasePathToImage("/favicon/favicon-16x16.png")}
       />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <link rel="manifest" href={applyBasePathToImage("/favicon/site.webmanifest")} />
       <link
         rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
+        href={applyBasePathToImage("/favicon/safari-pinned-tab.svg")}
         color="#000000"
       />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <link rel="shortcut icon" href={applyBasePathToImage("/favicon/favicon.ico")} />
       <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+      <meta name="msapplication-config" content={applyBasePathToImage("/favicon/browserconfig.xml")} />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
